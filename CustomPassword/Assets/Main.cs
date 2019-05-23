1049dcdfada81de2bd627a738dc102e08be5b5dc
Kjip <kjipgamer@gmail.com>
2019-05-23 18:53:58 +0200

    Added external process starting

---
+++ CustomPassword/Assets/Main.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using TMPro;
+
+public class Main : MonoBehaviour
+{
+
+    bool focussed;
+    bool passwordisright = false;
+    public string password;
+    public TMP_InputField passwordbar;
+
+    void Update()
+    {
+
+        if (passwordbar.text == password)
+            passwordisright = true;
+
+        if (!focussed && !passwordisright)
+        {
+            UnityEngine.Debug.Log("test");
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "test.bat";
+            Process.Start(startInfo);
+        }
+
+    }
+
+    private void OnApplicationFocus(bool focus) {
+        focussed = focus;
+    }
+
+}
